fb43266f9e38705457cee0e77cfc480ed0943336
Gustavo Casanova <gustavo.casanova@nicebots.com>
2020-09-02 04:43:00 -0300

    Implement simple ring buffer

--- tst-03/include/tst-03.h
+++ tst-03/include/tst-03.h
@@ -10,11 +10,23 @@
 #include <stdio.h>
 #include <time.h>
 
-// defines 
+// defines
+#define BUFFER_SIZE 8
+#define BUFFER_MASK (BUFFER_SIZE - 1)
+
+#define MAX_COUNT 255
 
 // Globals
+uint8_t buffer[BUFFER_SIZE];
+uint8_t buffer_byte_count = 0; /* Bytes received in buffer */
+uint8_t buffer_head = 0, buffer_tail = 0;
 
 // Prototypes
+void InitBuffer(void);
+void WriteBuffer(uint8_t data);
+uint8_t ReadBuffer(void);
+void PrintBuffer(void);
 void Delay(unsigned int milli_seconds);
+void ClrScr(void);
 
 #endif  // TST_03_H
--- tst-03/src/tst-03.c
+++ tst-03/src/tst-03.c
@@ -1,5 +1,5 @@
 // ---------------------------------------------
-// Test 02 - 2020-08-01 - Gustavo Casanova
+// Test 03 - 2020-08-01 - Gustavo Casanova
 // .............................................
 // Circular buffer test
 // ---------------------------------------------
@@ -9,12 +9,81 @@
 // Main function
 int main(void) {
 
-    printf("\r\nCircular buffer test\n\r");
+    InitBuffer();
+
+    ClrScr();
+    printf("Circular buffer test\n\r");
     printf("====================\n\n\r");
 
-    // printf("\n\rADC B: %d, Temperature calculation = %d\n\n\r", adc_temp, TempNTC(adc_temp, TO_CELSIUS, DT_CELSIUS));
-    for (;;) {};
+    Delay(1000);
+    ClrScr();
 
+    PrintBuffer();
+    Delay(2000);
+
+    for (uint8_t i = 1; i < MAX_COUNT + 1; i++) {
+        ClrScr();
+        WriteBuffer(i);
+        PrintBuffer();
+        Delay(250);
+    };
+}
+
+// Function BufferInit
+void InitBuffer(void) {
+    buffer_tail = 0;
+    buffer_head = 0;
+    buffer_byte_count = 0;
+}
+
+// Function WriteBuffer
+void WriteBuffer(uint8_t data) {
+    buffer_byte_count++;
+    buffer_head = ((buffer_head + 1) & BUFFER_MASK);
+    buffer[buffer_head] = data;
+}
+
+// Function ReadBuffer
+uint8_t ReadBuffer(void) {
+    //while (!rx_byte_count) {};                     // Wait until the buffer has data
+    buffer_tail = ((buffer_tail + 1) & BUFFER_MASK); // Update the buffer index
+    return buffer[buffer_tail];                      // Return data from the buffer
+}
+
+// void PrintBuffer
+void PrintBuffer(void) {
+    for (uint8_t i = 0; i < BUFFER_SIZE; i++) {
+        printf("| B%02d ", i);
+    }
+    printf("|\n\r-");
+    for (uint8_t i = 0; i < BUFFER_SIZE; i++) {
+        printf("------");
+    }
+    printf("\n\r");
+    for (uint8_t i = 0; i < BUFFER_SIZE; i++) {
+        if (buffer[i] < 10) {
+            printf("|   %d ", buffer[i]);
+        } else {
+            if (buffer[i] < 100) {
+                printf("|  %d ", buffer[i]);
+            } else {
+                printf("| %d ", buffer[i]);
+            }
+
+        }
+    }
+    printf("|\n\n\r");
+    for (int i = 0; i < buffer_head; i++) {
+        printf("      ");
+    }
+    printf(" head \n\r");
+    for (int i = 0; i < buffer_tail; i++) {
+        printf("      ");
+    }
+    printf(" tail \n\n\r");
+    // printf("Buffer head: %d\n\r", buffer_head);
+    // printf("Buffer tail: %d\n\r", buffer_tail);
+    printf("Buffer count: %d\n\r", buffer_byte_count);
 }
 
 // Function Delay
@@ -22,4 +91,10 @@ void Delay(unsigned int milli_seconds) {
     clock_t goal = milli_seconds + clock();
     while (goal > clock())
         ;
+}
+
+// Function ClrScr
+void ClrScr(void) {
+    //printf("\e[1;1H\e[2J");
+    system("clear");
 }
