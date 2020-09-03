42934e0d18650dd28b13362510c2939f2e9d20c0
Gustavo Casanova <gustavo.casanova@nicebots.com>
2020-09-03 10:24:43 -0300

    Clean up code

--- tst-03/src/tst-03.c
+++ tst-03/src/tst-03.c
@@ -8,7 +8,6 @@
 
 // Main function
 int main(void) {
-
     InitBuffer();
 
     ClrScr();
@@ -18,7 +17,6 @@ int main(void) {
     PrintBuffer();
     Delay(OPERATION_DELAY);
 
-    //for (uint8_t i = 1; i < MAX_COUNT; i++) {
     for (;;) {
         if (RandomByte(0, 1)) {
             // On true, write buffer
@@ -27,7 +25,6 @@ int main(void) {
                 // WriteBuffer(RandomByte(1, 255));
                 WriteBuffer(RandomByte(65, 122));
             } else {
-                // printf(" Buffer full, can't write data!\n\r");
                 printf(" *** FULL *** \n\r");
             }
         } else {
@@ -36,7 +33,6 @@ int main(void) {
                 printf(" [ READ ]\n\r");
                 ReadBuffer();
             } else {
-                // printf(" Buffer empty, can't read data!\n\r");
                 printf(" ___ EMPTY ___ \n\r");
             }
         }
@@ -44,16 +40,6 @@ int main(void) {
         ClrScr();
         PrintBuffer();
     }
-
-    //PrintBuffer();
-    //Delay(2000);
-    // for (uint8_t i = 1; i < MAX_COUNT + 1; i++) {
-    //     ClrScr();
-    //     WriteBuffer(i);
-    //     PrintBuffer();
-    //     Delay(OPERATION_DELAY);
-    // };
-
 }
 
 // Function BufferInit
@@ -70,18 +56,18 @@ void InitBuffer(void) {
 void WriteBuffer(uint8_t data) {
     buffer_head = ((buffer_head + 1) & BUFFER_MASK);
     buffer[buffer_head] = data;
-    buffer_byte_count++;    
+    buffer_byte_count++;
 }
 
 // Function ReadBuffer
 uint8_t ReadBuffer(void) {
     //while (!rx_byte_count) {};                     // Wait until the buffer has data
-    buffer_tail = ((buffer_tail + 1) & BUFFER_MASK); // Update the buffer index
+    buffer_tail = ((buffer_tail + 1) & BUFFER_MASK);  // Update the buffer index
 
-    buffer[buffer_tail] = 32; // Just for testing ...
+    buffer[buffer_tail] = 32;  // Just for testing ...
 
     buffer_byte_count--;
-    return buffer[buffer_tail];                      // Return data from the buffer
+    return buffer[buffer_tail];  // Return data from the buffer
 }
 
 // void PrintBuffer
@@ -98,17 +84,7 @@ void PrintBuffer(void) {
     printf("\n\r");
     // Print buffer contents
     for (uint8_t i = 0; i < BUFFER_SIZE; i++) {
-         printf("|  %c  ", buffer[i]);
-        // if (buffer[i] < 10) {
-        //     printf("|   %c ", buffer[i]);
-        // } else {
-        //     if (buffer[i] < 100) {
-        //         printf("|  %c ", buffer[i]);
-        //     } else {
-        //         printf("| %c ", buffer[i]);
-        //     }
-
-        // }
+        printf("|  %c  ", buffer[i]);
     }
     printf("|\n\n\r");
     // Print "head" pointer position
@@ -121,27 +97,25 @@ void PrintBuffer(void) {
         printf("      ");
     }
     printf(" tail \n\n\r");
-    // Print buffer counts
-    // printf("Buffer head: %d\n\r", buffer_head);
-    // printf("Buffer tail: %d\n\r", buffer_tail);
+    // Print buffer byte count
     printf("Buffer count: %d", buffer_byte_count);
 }
 
 // Function RandomByte
 uint8_t RandomByte(uint8_t lower, uint8_t upper) {
-    uint8_t random_byte = (rand() % (upper - lower + 1)) + lower; 
+    uint8_t random_byte = (rand() % (upper - lower + 1)) + lower;
     return random_byte;
-} 
+}
 
 // Function Delay
 void Delay(unsigned int milli_seconds) {
     clock_t goal = milli_seconds + clock();
-    while (goal > clock())
-        ;
+    while (goal > clock()) {
+    };
 }
 
 // Function ClrScr
 void ClrScr(void) {
-    //printf("\e[1;1H\e[2J");
-    system("clear");
+    //printf("\e[1;1H\e[2J"); // On serial terminal
+    system("clear");  // On PlatformIO terminal
 }
