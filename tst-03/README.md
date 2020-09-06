e9a3f3636c680f61a09f99fcb705a1181d94e802
Gustavo Casanova <gustavo.casanova@nicebots.com>
2020-09-06 10:34:56 -0300

    Add extra functions and modify comments

--- tst-03/README.md
+++ tst-03/README.md
@@ -1,4 +1,4 @@
 # Tst-03
-Circular buffer test.
+Simple circular queue test. The buffer size has to be a power of 2, since this value is used for the bitwise mask that makes the pointer go back to start.
 
 Platform: Native x86, Windows or Linux PC, GCC
--- tst-03/include/tst-03.h
+++ tst-03/include/tst-03.h
@@ -9,13 +9,14 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
 
 // defines
-#define BUFFER_SIZE 8
+#define BUFFER_SIZE 16
 #define BUFFER_MASK (BUFFER_SIZE - 1)
 
 #define MAX_COUNT 255
-#define OPERATION_DELAY 900000
+#define OPERATION_DELAY 250
 
 // Globals
 uint8_t buffer[BUFFER_SIZE];
