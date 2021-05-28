8e388ca556c0f00d35b212aa880a14c61d8a1993
Gustavo Casanova <gustavo.casanova@nicebots.com>
2021-05-28 19:46:35 -0300

    Set Tiny85 to make PB1 blink at 1-second intervals

--- tsts-atmel-studio/Tiny85-App-01/Tiny85-App-01/Tiny85-App-01.c
+++ tsts-atmel-studio/Tiny85-App-01/Tiny85-App-01/Tiny85-App-01.c
@@ -3,6 +3,14 @@
  *
  * Created: 12/05/2021 10:53:04
  *  Author: casanovg
+ *
+ * NOTE: Please make sure your ATtiny85's fuses are configured as shown below:
+ *       Low:      0x62
+ *       High:     0xD5
+ *       Extended: 0xFE
+ *
+ * This test is to verify that the chip is set properly.
+ * If so, a LED on PB1 should blink at (DELAY * 2) intervals.
  */ 
 
 #define F_CPU 1000000UL
@@ -14,11 +22,13 @@
 #define LED_DDR DDRB
 #define LED_PORT PORTB
 
+#define DELAY 500					/* Delay in milliseconds*/
+
 int main(void) {
     LED_DDR |= (1 << LED_PIN);		/* Set led control pin Data Direction Register for output */
     LED_PORT &= ~(1 << LED_PIN);	/* Turn led off */
     for(;;) {
 		LED_PORT ^= (1 << LED_PIN); /* Toggle PB1 */
-		_delay_ms(250);
+		_delay_ms(DELAY);
     }
 }
